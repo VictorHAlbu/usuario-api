ebaeb72cf427b3540b4206b65801778aedf71a45
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-04-16 11:50:14 -0300

    Persistindo dados no mongo

--- app/controllers/adm_controller.js
+++ app/controllers/adm_controller.js
@@ -1,10 +1,23 @@
 var Adm = require('../models/adm.js');
 
 const AdmController = {
+
     index: (req, res, next) => {
-        Adm.find().then(dado =>{
-        res.send(dado);
-    });   
+
+        Adm.find().then(dado => res.send(dado));   
+
+    //     const adm = new Adm({ nome: 'Suport', senha: '123456', email: 'suport@gmail.com' });
+    //     adm.save(error => {
+        
+    //     if(error){
+    //         res.send(error);
+    //         return
+    //     }
+
+    //       Adm.find().then(dado => {
+    //         res.send(dado);
+    //     });   
+    //  });
     }
 }
 
