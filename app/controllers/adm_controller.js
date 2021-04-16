10fd1254269edeb8b62b8848a9a6a0df95de53fa
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-04-16 14:56:46 -0300

    Finalizando a API

--- app/controllers/adm_controller.js
+++ app/controllers/adm_controller.js
@@ -18,7 +18,46 @@ const AdmController = {
     //         res.send(dado);
     //     });   
     //  });
+    },
+    create: (req, res, next) => {   
+        const adm = new Adm({ nome: res.body.nome, senha: res.body.senha, email: res.body.email });
+        adm.save(error => {
+        
+        if(error){
+            res.send(error, 401);
+            return
+        }
+          res.send({}, 201);   
+    
+        });
+    },
+    change: (req, res, next) => {   
+        Adm.find({_id: req.params.adm_id}).then(dado => {
+            if(dado.length > 0){
+                adm =  adm[0];
+                adm.nome = res.body.nome
+                adm.senha = res.body.senha
+                adm.email = res.body.email
+                adm.save(error => {
+                    if(error){
+                        res.send(error, 401);
+                        return
+                    }
+                      res.send(adm, 200);   
+                });
+            }
+        });   
+    },
+    delete: (req, res, next) => {   
+        Adm.deleteMany({_id: req.params.adm_id}).then(data => {
+            if(data.deletedCount === 0) {
+                res.send(data, 401);
+                return
+            }
+              res.send(adm, 200);
+        });   
     }
+    
 }
 
 module.exports = AdmController;
