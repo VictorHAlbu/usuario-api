50092a06c9720565eac5b3816948f3ebf3f548e0
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-04-18 20:11:11 -0300

    Api com segurança Token headers

--- app/controllers/adm_controller.js
+++ app/controllers/adm_controller.js
@@ -1,63 +1,61 @@
-var Adm = require('../models/adm.js');
+const Adm = require('../models/adm');
+const TOKEN = "12345213455";
 
 const AdmController = {
+  index: (req, res, next) => {
 
-    index: (req, res, next) => {
-
-        Adm.find().then(dado => res.send(dado));   
-
-    //     const adm = new Adm({ nome: 'Suport', senha: '123456', email: 'suport@gmail.com' });
-    //     adm.save(error => {
-        
-    //     if(error){
-    //         res.send(error);
-    //         return
-    //     }
-
-    //       Adm.find().then(dado => {
-    //         res.send(dado);
-    //     });   
-    //  });
-    },
-    create: (req, res, next) => {   
-        const adm = new Adm({ nome: res.body.nome, senha: res.body.senha, email: res.body.email });
-        adm.save(error => {
-        
-        if(error){
-            res.send(error, 401);
-            return
-        }
-          res.send({}, 201);   
-    
-        });
-    },
-    change: (req, res, next) => {   
-        Adm.find({_id: req.params.adm_id}).then(dado => {
-            if(dado.length > 0){
-                adm =  adm[0];
-                adm.nome = res.body.nome
-                adm.senha = res.body.senha
-                adm.email = res.body.email
-                adm.save(error => {
-                    if(error){
-                        res.send(error, 401);
-                        return
-                    }
-                      res.send(adm, 200);   
-                });
-            }
-        });   
-    },
-    delete: (req, res, next) => {   
-        Adm.deleteMany({_id: req.params.adm_id}).then(data => {
-            if(data.deletedCount === 0) {
-                res.send(data, 401);
-                return
-            }
-              res.send(adm, 200);
-        });   
+    if(req.headers.token == TOKEN){
+      Adm.find().then(dado => res.send(dado) );
     }
-    
-}
+    else{
+      res.status(401).send({error: "Acesso negado a API, Token invalido"});
+    }
+  },
+  create: (req, res, next) => {
+  if(req.headers.token == TOKEN){
+    const adm = new Adm({ nome: req.body.nome, senha: req.body.senha, email: req.body.email });
+    adm.save(error => {
+      if(error){
+        res.status(401).send(error)
+        return
+      }
+      
+      res.status(201).send({});
+    });
+  }
+  else{
+    res.status.send({error: `Acesso negado a API`})
+  }
+  },
+  change: async(req, res, next) => {
+  if(req.headers.token == TOKEN){
+    try{
+      await Adm.findOneAndUpdate({_id: req.params.adm_id}, {nome: req.body.nome, senha: req.body.senha, email: req.body.email})
+      res.status(204).send(`Alterado com o id ${req.params.adm_id}`)
+    }
+    catch(err){
+      res.status(401).send(`Erro: ${err}`)
+    }
+  }
+  else{
+    res.status(401).send({error:`Acesso negado a API`})
+  }
+  }
+  ,
+  delete: async(req, res, next) => {
+  if(req.headers.token == TOKEN){
+    try{
+      await Adm.findByIdAndDelete(req.params.adm_id)
+      res.status(204).send({});
 
+    }
+    catch(err){
+      res.status(401).send({})
+    }
+  }
+  else{
+    res.status(401).send({error: "Acesso negado a API"})
+  }
+  }
+}
 module.exports = AdmController;
